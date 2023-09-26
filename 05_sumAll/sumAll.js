47bbb0af15cf30eb8d4ea20d63a8050ed491935a
drearydev <143649090+DrearyDev@users.noreply.github.com>
2023-09-26 16:29:26 -0400

    Add solution for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let number = typeof 3;
+    if (typeof num1 !== number || typeof num2 !== number || num1 < 0 || num2 < 0){
+        return 'ERROR';
+    };
+
+    let greater, smaller;
+    (num1 > num2) ? greater = num1 : greater = num2;
+    if (greater === num1) smaller = num2;
+    else smaller = num1;
+
+    let sum = 0;
+
+    for (let i = smaller; i < greater; i++){
+        sum += i;
+    };
+    
+    return (sum + greater);
 
 };
-
 // Do not edit below this line
 module.exports = sumAll;
