550d4a85ed3e9b4df151d4b36bc8131a1978f72d
drearydev <143649090+DrearyDev@users.noreply.github.com>
2023-09-26 15:31:43 -0400

    Add solution for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
+    let array = arguments[0];
+    for (let i = 1; i < arguments.length; i++){
+        array.forEach(element => {
+            if (element === arguments[i]){
+                const index = array.indexOf(arguments[i]);
 
+                array.splice(index, 1);
+            };
+        });
+    };
+    return array;
 };
 
 // Do not edit below this line
